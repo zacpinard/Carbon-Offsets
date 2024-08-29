72aecb59abecaab1cdb10b54b4efe14b6c206d1a
zacpinard <zacpinard@gmail.com>
2024-08-28 20:41:10 -0700

    main copy 2 test

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ function createMap() {
         maxZoom: 19,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
+    
     /*
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
